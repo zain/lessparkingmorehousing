150b97f6bdb9a9e5c91fcfac2b4159baa7c4889f
Zain Memon <zain@inzain.net>
2017-06-16 21:49:24 -0700

    request.body instead of request.POST

--- mapdraw/views.py
+++ mapdraw/views.py
@@ -5,7 +5,7 @@ from mapdraw.models import ParkingSpot
 
 def new_poly(request):
     if request.method == "POST":
-        geojson = request.POST['geojson']
+        geojson = request.body.decode('utf-8')
         ParkingSpot.objects.create(shape=geojson)
         return HttpResponse('ok', content_type="text/plain")
     return HttpResponse("why don't you POST here", content_type="text/plain")
