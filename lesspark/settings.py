9be866df2712295df19224077816414bd28ef508
Zain Memon <zain@inzain.net>
2017-06-16 20:18:59 -0700

    postgis

--- lesspark/settings.py
+++ lesspark/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
