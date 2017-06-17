3803ee9e09c78595e02ae3636c032feccd85da8c
Zain Memon <zain@inzain.net>
2017-06-17 14:17:51 -0700

    fix new_poly

--- mapdraw/admin.py
+++ mapdraw/admin.py
@@ -2,6 +2,6 @@ from django.contrib.gis import admin
 from mapdraw.models import ParkingSpot
 
 
-class ParkingSpotAdmin(admin.GeoModelAdmin):
+class ParkingSpotAdmin(admin.OSMGeoAdmin):
     pass
 admin.site.register(ParkingSpot, ParkingSpotAdmin)
--- mapdraw/views.py
+++ mapdraw/views.py
@@ -1,6 +1,7 @@
 from random import randint
+import json
 
-from django.contrib.gis.geos import Polygon
+from django.contrib.gis.geos import GEOSGeometry
 from django.http import HttpResponse
 import mercantile
 
@@ -8,13 +9,12 @@ from mapdraw.models import ParkingSpot
 
 
 def new_poly(request):
-    # if request.method == "POST":
-    #     geojson = request.body.decode('utf-8')
-    #     ParkingSpot.objects.create(geojson=geojson)
-    #     return HttpResponse('ok', content_type="text/plain")
-    ext_coords = ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0))
-    poly = Polygon(ext_coords)
-    ParkingSpot.objects.create(poly=poly)
+    if request.method == "POST":
+        feature_collection = json.loads(request.body.decode('utf-8'))
+        for feature in feature_collection['features']:
+            poly = GEOSGeometry(json.dumps(feature['geometry']))
+            ParkingSpot.objects.create(poly=poly)
+        return HttpResponse('ok', content_type="text/plain")
 
     return HttpResponse("why don't you POST here", content_type="text/plain")
 
