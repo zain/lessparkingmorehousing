c8389a1cda13d9d3a049f2778e01aa293c3f04e7
Zain Memon <zain@inzain.net>
2017-06-16 21:51:03 -0700

    working new_poly endpoint

--- mapdraw/views.py
+++ mapdraw/views.py
@@ -6,6 +6,6 @@ from mapdraw.models import ParkingSpot
 def new_poly(request):
     if request.method == "POST":
         geojson = request.body.decode('utf-8')
-        ParkingSpot.objects.create(shape=geojson)
+        ParkingSpot.objects.create(geojson=geojson)
         return HttpResponse('ok', content_type="text/plain")
     return HttpResponse("why don't you POST here", content_type="text/plain")
